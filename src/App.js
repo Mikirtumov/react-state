ef1bd55ba89a710ceab7b55bec91eaef7d2490ba
Mikirtumov <you@example.com>
2020-05-24 23:53:38 -0700

    buttons working

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import './App.css';
 
 function App() {
 
-  const [count, setCount] = useState(10);
+    const [count, setCount] = useState(10);
 
-  const clickerPlus = () => {
-    setCount (count + 1)
-  }
+    const clickerPlus = () => {
+        setCount(count + 1)
+    }
+    const clickerMinus = () => {
+        setCount(count - 1)
+    }
 
-  return (
-    <div className="App">
-  <button type="button" className="btn btn-danger" onClick={clickerPlus}>PLUS</button>
-      <hr/>
-      {count}
-    </div>
-  );
+    return (
+        <div className="App">
+            <button type="button" className="btn btn-danger" onClick={clickerPlus}>PLUS</button>
+            <hr/>
+            {count}
+            <hr/>
+            <button type="button" className="btn btn-success" onClick={clickerMinus}>MINUS</button>
+        </div>
+    );
 }
 
 export default App;
