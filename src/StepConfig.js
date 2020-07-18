53f48691563ad87cb0f482114c2be7316cba21c0
Sergo Mikirtumov <mikirtumov@gmail.com>
2020-07-18 11:33:21 -0700

    removed alert

--- src/StepConfig.js
+++ src/StepConfig.js
@@ -7,7 +7,6 @@ function StepConfig(props) {
     return (
         <div className="h2">
             <input onChange={(e) => props.onStepChange(+e.target.value)} />
-            <button  onClick={() => (alert("saeors"))}>Step</button>
         </div>
     );
 }
